aa47de07d006c88945abc7da7f30a6d424118049
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2019-05-17 01:51:18 +0900

    add map and tslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,76 @@
 import React from 'react';
 import './App.css';
-const { useState, useEffect } = React;
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet'
+
+const {useState, useEffect} = React;
 
 const fetchEvents = async ({number = 20, country = 'JP'} = {}) => {
   const api = `https://api.wordpress.org/events/1.0/?country=${country}&number=${number}`
   const response = await fetch(api)
   const {events} = await response.json()
-  console.log(events)
   return events
+}
+
+type Location = {
+  location: string
+  country: string
+  latitude: number
+  longitude: number
+}
+
+type MeetupEvent = {
+  date: string
+  location: Location
+  meetupEvent: string
+  meetupEvent_url: string
+  title: string
+  type: string
+  url: string
+}
+
+const EventsMap: React.FC<{ meetupEvents: MeetupEvent[] }> = ({meetupEvents}) => {
+  if (meetupEvents.length > 0) {
+    const bounds: [number, number][] = meetupEvents.map((meetup: MeetupEvent) => {
+      const {latitude, longitude} = meetup.location;
+      return [
+        latitude,
+        longitude
+      ]
+    })
+    return (
+      <Map bounds={bounds}>
+        <TileLayer
+          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {meetupEvents.map((meetupEvent: MeetupEvent, i: number) => (
+          <Marker key={i} position={[meetupEvent.location.latitude, meetupEvent.location.longitude]}>
+            <Popup>
+              A pretty CSS3 popup. <br/> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
+      </Map>
+    )
+  }
+
+  return (<div />)
 }
 
 
 const App: React.FC = () => {
-  const [meetups, setMeetups] = useState<Array<any>>([]);
-
+  const [meetupEvents, setMeetupEvents] = useState<Array<MeetupEvent>>([]);
   useEffect(() => {
-    fetchEvents().then(setMeetups)
-  }, [meetups]);
-
+    fetchEvents().then(setMeetupEvents)
+  }, [meetupEvents]);
   return (
     <div className="App">
-      {meetups.map((meetup:any, i:number) => (
+      <EventsMap meetupEvents={meetupEvents}/>
+      {meetupEvents.map((meetupEvent: MeetupEvent, i: number) => (
         <div key={i}>
-          <div><a href={meetup.url}>{meetup.title}</a></div>
-          <time>{meetup.date}</time>
-          <div><a href={meetup.meetup_url}>{meetup.meetup}</a></div>
+          <div><a href={meetupEvent.url}>{meetupEvent.title}</a></div>
+          <time>{meetupEvent.date}</time>
+          <div><a href={meetupEvent.meetupEvent_url}>{meetupEvent.meetupEvent}</a></div>
         </div>
       ))}
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Leaflet from 'leaflet'
+import 'leaflet/dist/leaflet.css';
+Leaflet.Icon.Default.imagePath = '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
