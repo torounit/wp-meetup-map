f9637757ac2844e93a41f98a0d20a2a7aa9bec32
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2019-05-17 00:36:32 +0900

    add url

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import './App.css';
 const { useState, useEffect } = React;
 
 const fetchEvents = async ({number = 20, country = 'JP'} = {}) => {
-  console.log('fetch');
   const api = `https://api.wordpress.org/events/1.0/?country=${country}&number=${number}`
   const response = await fetch(api)
   const {events} = await response.json()
+  console.log(events)
   return events
 }
 
@@ -15,17 +15,14 @@ const App: React.FC = () => {
   const [meetups, setMeetups] = useState<Array<any>>([]);
 
   useEffect(() => {
-    fetchEvents().then( (result) => {
-      console.log(result)
-      setMeetups(result)
-    })
-  });
+    fetchEvents().then(setMeetups)
+  }, [meetups]);
 
   return (
     <div className="App">
       {meetups.map((meetup:any, i:number) => (
         <div key={i}>
-          <div> {meetup.title}</div>
+          <div><a href={meetup.url}>{meetup.title}</a></div>
           <time>{meetup.date}</time>
           <div><a href={meetup.meetup_url}>{meetup.meetup}</a></div>
         </div>
