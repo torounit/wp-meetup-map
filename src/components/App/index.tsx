0951790e12d33be0bda06bd5f6498dc61a6a1173
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2019-07-11 16:30:41 +0900

    use React Hooks

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,31 +1,33 @@
 import React from "react"
 import CountrySelect from "../CountrySelect"
-import {MeetupEvent} from "../Events"
-import {fetchCountryCode, fetchEvents} from "../../api"
+import { MeetupEvent } from "../Events"
+import { fetchCountryCode, fetchEvents } from "../../api"
 import config from "../../config"
 import classes from "./index.module.css"
-import {getCountryFromLocalStorage, getCurrentPosition, parseQuery, setCountryFromLocalStorage} from "../../utility"
-import Notes from "./Notes";
-import MeetupSelect from "./MeetupSelect";
-import EventsView from "./EventsView";
+import { getCountryFromLocalStorage, getCurrentPosition, parseQuery, setCountryFromLocalStorage } from "../../utility"
+import Notes from "./Notes"
+import MeetupSelect from "./MeetupSelect"
+import EventsView from "./EventsView"
 
-const {useState, useEffect} = React
-const {defaultCountry} = config
+const { useState, useEffect } = React
+const { defaultCountry } = config
 
 const getCountryCode = (): string => {
   const query = parseQuery()
   if (query.country && typeof query.country === "string") {
     return query.country.toUpperCase()
   }
-  const country = getCountryFromLocalStorage();
+  const country = getCountryFromLocalStorage()
   if (country) {
     return country
   }
-  return ''
+  return ""
 }
 
-
-const Index: React.FC = () => {
+/**
+ * Hooks for country code.
+ */
+const useCountryState = (): [string, (state: string) => void] => {
   const [country, setCountry] = useState<string>(getCountryCode() || defaultCountry)
 
   const saveCountry = (state: string) => {
@@ -35,24 +37,31 @@ const Index: React.FC = () => {
 
   useEffect(() => {
     (async () => {
-      let code = getCountryCode();
+      let code = getCountryCode()
       if (code) {
         saveCountry(code)
-      }
-      else {
+      } else {
         const position = await getCurrentPosition()
         code = await fetchCountryCode(position.coords.latitude, position.coords.longitude)
         saveCountry(code)
       }
-
     })()
   }, [])
+  return [country, saveCountry]
+}
+
+/**
+ * App Components.
+ * @constructor
+ */
+const Index: React.FC = () => {
+  const [country, setCountry] = useCountryState()
 
   const [meetupEvents, setMeetupEvents] = useState<Array<MeetupEvent>>([])
   useEffect(() => {
     (async () => {
       if (country) {
-        const events = await fetchEvents({country})
+        const events = await fetchEvents({ country })
         setMeetupEvents(events)
       }
     })()
@@ -61,14 +70,14 @@ const Index: React.FC = () => {
   const [meetup, setMeetup] = useState<string>("")
   useEffect(() => setMeetup(""), [meetupEvents])
 
-  const [selectedEvents, setSelectedEvents] = useState<Array<MeetupEvent>>([])
 
+  const [selectedEvents, setSelectedEvents] = useState<Array<MeetupEvent>>([])
   useEffect(() => {
-    const filteredEvents = meetupEvents.filter((meetupEvent) => {
+    const filteredEvents = meetupEvents.filter(meetupEvent => {
       if (!meetup) {
         return true
       }
-      return meetupEvent.meetup && meetupEvent.meetup.includes(meetup);
+      return meetupEvent.meetup && meetupEvent.meetup.includes(meetup)
     })
     setSelectedEvents(filteredEvents)
   }, [meetup, meetupEvents])
@@ -82,17 +91,16 @@ const Index: React.FC = () => {
         <h1 className={classes.title}>WordPress Meetup Map</h1>
         <button className={classes.notesButton} onClick={() => setShowNotes(!showNotes)}>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <path d="M0 0h24v24H0z" fill="none"/>
-            <path
-              d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/>
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
           </svg>
           <span className={classes.screenReaderText}>notes</span>
         </button>
-        <CountrySelect label="Country:" className={classes.countrySelect} value={country} onChange={saveCountry}/>
-        <MeetupSelect label="Meetup:" meetupEvents={meetupEvents} onChange={(e) => setMeetup(e.target.value)}/>
+        <CountrySelect label="Country:" className={classes.countrySelect} value={country} onChange={setCountry} />
+        <MeetupSelect label="Meetup:" meetupEvents={meetupEvents} onChange={e => setMeetup(e.target.value)} />
       </header>
       <main className={classes.main}>
-        <EventsView meetupEvents={selectedEvents}/>
+        <EventsView meetupEvents={selectedEvents} />
       </main>
       <footer className={classes.footer}>
         <div className={classes.menu}>
@@ -100,7 +108,7 @@ const Index: React.FC = () => {
           <button onClick={() => setMode("events")}>Events</button>
         </div>
       </footer>
-      {showNotes && <Notes onClose={() => setShowNotes(false)}/>}
+      {showNotes && <Notes onClose={() => setShowNotes(false)} />}
     </div>
   )
 }
