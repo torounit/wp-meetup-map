3d99f64bbabd1cc12537754b9f83dfcf9e6cc195
torounit <mail@torounit.com>
2026-04-02 03:45:08 +0900

    Migrate to Vite & React 18, switch to pnpm

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client"
 import "./index.css"
 import Index from "./components/App"
-import * as serviceWorker from "./serviceWorker"
 import Leaflet from "leaflet"
 import "leaflet/dist/leaflet.css"
-Leaflet.Icon.Default.imagePath = "//cdn.jsdelivr.net/npm/leaflet@1.5.1/dist/images/"
-ReactDOM.render(<Index />, document.getElementById("root"))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+Leaflet.Icon.Default.imagePath = "//cdn.jsdelivr.net/npm/leaflet@1.5.1/dist/images/"
+
+const container = document.getElementById("root")
+if (container) {
+  const root = createRoot(container)
+  root.render(<Index />)
+}
---
+++ vite.config.ts
@@ -0,0 +1,63 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/api\.wordpress\.org\/.+/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'wp-org-api',
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.+/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'jsdelivr',
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24
+              }
+            }
+          }
+        ]
+      },
+      manifest: {
+        short_name: "WP Meetup Map",
+        name: "WordPress Meetup Map",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon"
+          },
+          {
+            src: "icon-192x192.png",
+            type: "image/png",
+            sizes: "192x192"
+          },
+          {
+            src: "icon-512x512.png",
+            type: "image/png",
+            sizes: "512x512"
+          }
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#F64060",
+        background_color: "#ffffff"
+      }
+    })
+  ]
+})
